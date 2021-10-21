70d13d1fe07725f309c54b422e572164e6a7e530
Jose Vera <djjor3l@gmail.com>
2021-10-21 12:42:53 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./animation.scss";
+import Type from "./Type";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="monitor">
+      <section className="screen">
+        <div className="content on">
+          <Type speed={5} delay={3000}>
+            <span className="line">
+              Welcome to JoseOS LTS 0.2.0 (GNU/Linux 4.4.0-141-generic x86_64)
+            </span>
+            <br />
+            <br />
+            <span className="line">
+              [ OK ] Started Show Plymouth Boot Screen.
+            </span>
+            <span className="line">
+              [ OK ] Started Forward Password Requests to Plymouth Directory
+              Watch.
+            </span>
+            <span className="line">
+              [ OK ] Reached target Encrypted Volumes.
+            </span>
+            <span className="line">[ OK ] Reached target Paths.</span>
+            <span className="line">[ OK ] Found device /dev/serial1.</span>
+            <span className="line">[ OK ] Found device /dev/ttyS0.</span>
+            <span className="line">.</span>
+            <span className="line">.</span>
+            <span className="line">[ OK ] Reached target Sockets.</span>
+            <span className="line">[ OK ] Reached target Basic System.</span>
+            <span className="line">
+              Starting triggerhappy global hotkey daemon...
+            </span>
+            <span className="line">Starting dhcpcd on all interfaces...</span>
+            <span className="line">Starting Avahi mDNS/DNS-SD Stack...</span>
+            <span className="line">Starting rng-tools.service...</span>
+            <span className="line">
+              Starting LSB: Switch to ondemand cpuâ€¦or (unless shift key is
+              pressed)...
+            </span>
+            <span className="line">Starting System Logging Service...</span>
+            <span className="line">Starting Login Service...</span>
+            <span className="line">
+              Starting Disable WiFi if country not set...
+            </span>
+            <span className="line">
+              [ OK ] Started Daily apt download activities.
+            </span>
+            <span className="line">
+              [ OK ] Started Daily apt upgrade and clean activities.
+            </span>
+            <span className="line">[ OK ] Reached target Timers.</span>
+            <span className="line">.</span>
+            <span className="line">.</span>
+            <span className="line">
+              [ OK ] Started triggerhappy global hotkey daemon.
+            </span>
+            <span className="line">[ OK ] Started System Logging Service.</span>
+            <span className="line">
+              [ OK ] Started dhcpcd on all interfaces.
+            </span>
+            <span className="line">[ OK ] Started rng-tools.service.</span>
+            <span className="line">
+              [ OK ] Started Disable WiFi if country not set.
+            </span>
+            <span className="line">[ OK ] Started Login Service.</span>
+          </Type>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/Type.js
@@ -0,0 +1,69 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState } from "react";
+import striptags from "striptags";
+
+export default function Type({ speed, delay, children }) {
+  const [typed, setTyped] = useState([]);
+  const lineRef = useRef(null); //useCallback((node) => setLine(node), []);
+
+  const typeString = async () => {
+    const line = lineRef.current;
+    if (!line) return;
+
+    const str = line.textContent;
+    line.textContent = "";
+    line.style.display = "block";
+    let cleanStr = striptags(str);
+
+    for (const letter of cleanStr) {
+      await new Promise((resolve, reject) => {
+        if (line) {
+          line.textContent += letter;
+          setTimeout(() => {
+            resolve();
+          }, speed || 10);
+        }
+      });
+    }
+
+    typeLine(); // next one
+  };
+
+  const typeLine = (_) => {
+    console.log(children.length, typed.length);
+    if (children.length > typed.length) {
+      let line = children[typed.length];
+      const clone = React.cloneElement(line, {
+        ref: lineRef,
+        key: Math.random() * 1000,
+        style: {
+          display: "none",
+        },
+      });
+
+      setTyped([
+        ...typed.map((e) =>
+          React.cloneElement(e, {
+            ref: null,
+            style: {
+              display: "block",
+            },
+          })
+        ),
+        clone,
+      ]);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      typeLine();
+    }, delay);
+  }, []);
+
+  useEffect(async () => {
+    await typeString();
+  }, [typed.length]);
+
+  return <div key={Math.random() * 1000}>{typed}</div>;
+}
