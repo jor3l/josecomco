5482c68c9f2b72117b8a272cbc0cdc879a34cd58
Jose <djjor3l@gmail.com>
2023-11-15 21:06:42 -0500

    update nov2023

--- src/components/DesktopWindow.js
+++ src/components/DesktopWindow.js
@@ -1,25 +1,46 @@
-export default ({title, children}) => {
-    return <div class="container">
-    <div class="title">
-      <div class="pull-right">
-        <button class="minimize"><span class="fa fa-minus"></span></button>
-        <button class="expand"><span class="fa fa-square-o"></span></button>
-        <button class="times"><span class="fa fa-times"></span></button>
+export default ({ title, children }) => {
+  return (
+    <div class="container">
+      <div class="title">
+        <div class="pull-right">
+          <button class="minimize">
+            <span class="fa fa-minus"></span>
+          </button>
+          <button class="expand">
+            <span class="fa fa-square-o"></span>
+          </button>
+          <button class="times">
+            <span class="fa fa-times"></span>
+          </button>
+        </div>
+        <h1>
+          <div class="icon-my-computer"></div> {title || "New file"}
+        </h1>
       </div>
-      <h1><div class="icon-my-computer"></div> {title || 'New file'}</h1>
+      <ul class="toolbar">
+        <li>
+          <u>F</u>ile
+        </li>
+        <li>
+          <u>E</u>dit
+        </li>
+        <li>
+          <u>V</u>iew
+        </li>
+        <li>
+          <u>H</u>elp
+        </li>
+      </ul>
+      <div class="container-inner">{children}</div>
+      <div class="statusbar">
+        <div
+          class="left"
+          title="this website was created with resources freely avaiable on codepen and gifcities"
+        >
+          2023.
+        </div>
+        <div class="right">Last saved by Jose Vera on Nov 15, 2023.</div>
+      </div>
     </div>
-    <ul class="toolbar">
-      <li><u>F</u>ile</li>
-      <li><u>E</u>dit</li>
-      <li><u>V</u>iew</li>
-      <li><u>H</u>elp</li>
-    </ul>
-    <div class="container-inner">
-        {children}
-    </div>
-    <div class="statusbar">
-      <div class="left" title="this website was created with resources freely avaiable on codepen and gifcities">2022.</div>
-      <div class="right">Last saved by Jose Vera.</div>
-    </div>
-  </div>
-}
+  );
+};
--- src/views/portfolio.js
+++ src/views/portfolio.js
@@ -9,7 +9,7 @@ import image4 from "../img/image4.png";
 import image5 from "../img/image5.png";
 import image6 from "../img/image6.png";
 import welcome from "../img/welcome4.gif";
-import hand from "../img/haut_page.gif"
+import hand from "../img/haut_page.gif";
 import newGif from "../img/new.gif";
 import phone from "../img/phone.gif";
 import email from "../img/email.gif";
@@ -22,57 +22,64 @@ export default () => {
         <DesktopWindow title="portfolio.rtf">
           <h1>Hey!, welcome to my website!</h1>
           <br />
-            <hr /><br />
-          <p align="center"><img
-                      alt="WELCOME!"
-                      src={welcome}
-                    /></p>
-                    <br />
-            <hr />
+          <hr />
+          <br />
+          <p align="center">
+            <img alt="WELCOME!" src={welcome} />
+          </p>
+          <br />
+          <hr />
           <p>
             I'm <span class="flash">OPEN FOR WORK</span>, keep reading to find
             out more about me and if interested, send me a{" "}
-            <a href="https://wa.me/573044503724/">Whatsapp!<img
-                      alt="Phone"
-                      src={phone}
-                      width="60px"
-                    /></a> 
-                     OR <a href="mailto:ceo@bugs.com.co"><img
-                    alt="EMAIL ME"
-                    src={email}
-                  /></a>
+            <a href="https://wa.me/573044503724/">
+              Whatsapp!
+              <img alt="Phone" src={phone} width="60px" />
+            </a>
+            OR{" "}
+            <a href="mailto:ceo@bugs.com.co">
+              <img alt="EMAIL ME" src={email} />
+            </a>
             <br />
             <br />
             <hr />
-            <p align="center"><img
-                      alt="IMPORTANT!"
-                      src={hand}
-                    /></p>
+            <p align="center">
+              <img alt="IMPORTANT!" src={hand} />
+            </p>
             <br />
             <h2>About</h2>
             <br />
             <p>
-              Self taught full stack developer with more than 12 years of
-              experience in mobile apps, web apps and games and web+hardware,
-              IOT and home automation. I'm into 3d printing and 3d functional
-              design as a hobby. Extensive experience in development using a
-              wide variety of tools and languages both for the front and the
-              back, including but not limited to PHP, JavaScript and Ruby on
-              Rails. Lots of experience with UI tools and frameworks, mostly
-              (React). Loving GraphQL but also experienced in consuming Rest
-              APIs and SOAP services.
+              I am a seasoned full stack engineer with a background in web
+              development, boasting over 10 years of experience in the field.
+              Previously, I held the position of a full stack engineer at
+              Skillshare, a prominent online learning platform that facilitates
+              connections between millions of learners and teachers globally.
             </p>
-            <br />
             <p>
-              I've been developing web platforms of different sizes, e-commerce
-              websites with online payments integration, web and mobile games,
-              Arduino Projects, Hardware-to-web interfaces, AI/VR experiences,
-              offline web apps and many other things, I always try to keep
-              myself busy with a new project or idea. Experience with the Adobe
-              Suite and Video editing software such as: Photoshop, Fireworks,
-              Illustrator, After Effects, Premiere and DaVinci Resolve.
+              During my time at Skillshare, I played a pivotal role in enhancing
+              the user experience and delivering value to clients. My
+              responsibilities included refining solutions for external clients,
+              particularly those offering Skillshare as a perk to their
+              employees. I utilized my extensive expertise in PHP, JavaScript,
+              React, and various cloud-based tools to migrate services to a
+              Dockerized environment, develop new features and tools, and
+              contribute to the growth of revenue and platform usage.
             </p>
+            <p>
+              Collaborating seamlessly with a cross-functional team of
+              engineers, designers, and product managers, I ensured the quality
+              and performance of the solutions delivered. My overarching goal
+              was to contribute to Skillshare's mission of inspiring creativity,
+              curiosity, and lifelong learning.
+            </p>
+            As of Dec 2023, I find myself in a transitional phase after a recent
+            layoff from Skillshare. I am actively seeking new opportunities to
+            apply my skills and expertise in a dynamic and innovative
+            environment, continuing to contribute to the ever-evolving landscape
+            of web development.
           </p>
+
           <br />
           <br />
           <table border="1" cellPadding="5">
@@ -115,10 +122,7 @@ export default () => {
               </td>
               <td class="c2" colspan="1" rowspan="1">
                 <h3 class="c4" id="h.vq1a7a33ikzj">
-                <img
-                      alt="NEW"
-                      src={newGif}
-                    />
+                  <img alt="NEW" src={newGif} />
                   <span class="c26 c20">Staff Engineer at Skillshare</span>
                 </h3>
                 <p class="c11">
@@ -126,7 +130,7 @@ export default () => {
                 </p>
                 <p class="c11">
                   <span class="c10">Sep</span>
-                  <span class="c6">&nbsp;2021 &ndash; Aug 2022</span>
+                  <span class="c6">&nbsp;2021 &ndash; Nov 2023</span>
                 </p>
                 <p class="c11">
                   <span class="c6">US (remote)</span>
@@ -134,25 +138,37 @@ export default () => {
                 <br />
                 <p class="c11">
                   <span class="c5">
-                    Joined Skillshare as a Staff (full stack) engineer to help
-                    improve the solutions that the company offers to external
-                    clients and companies.{" "}
+                    As a Staff (full stack) Engineer at Skillshare, I was
+                    responsible for improving the company's solutions for
+                    external clients, including companies that offer Skillshare
+                    as a perk to their employees. In this role, I leveraged my
+                    expertise to enhance the platform's capabilities and provide
+                    better experiences for users.
                   </span>
                 </p>
                 <br />
                 <p class="c11">
                   <span class="c5">
-                    At Skillshare I have been able to work closely with multiple
-                    cloud based tools, migrate services to a Dockerized
-                    environment and create new and exciting features and tools
-                    to increase revenue and platform usage.
+                    As a member of Skillshare's Enterprise team, I worked
+                    closely with various cloud-based tools, migrating services
+                    to a Dockerized environment and creating new features and
+                    tools to increase revenue and usage of the platform. Using
+                    PHP for the backend and JavaScript for developing new
+                    services, I was able to deliver effective solutions that met
+                    the needs of the project.
                   </span>
                 </p>
                 <br />
                 <p class="c11">
                   <span class="c8">
-                    Been using PHP for the backend and JS to develop new
-                    services.
+                    Currently, I'm part of the discovery team, where I focus on
+                    providing solutions to help users find the right content
+                    more easily. In this role, I work to develop tools that
+                    streamline the search process and better understand users'
+                    needs through data and machine learning. My contributions to
+                    the discovery team have helped Skillshare to improve its
+                    user experience and provide better solutions for its
+                    customers.
                   </span>
                 </p>
               </td>
@@ -214,15 +230,32 @@ export default () => {
                 </p>
                 <p class="c11">
                   <span class="c8">
-                    Bunch is a virtual coach that offers curated content to
-                    anyone looking to improve and learn how to be a better
-                    leader. I worked with them on the redesign of the backend to
-                    support their new and improved app, this included
-                    performance fixes and general improvements to the platform.
-                    <br />
-                    <br />
-                    Main stack was NodeJS + TS and heavily focused on
-                    microservices and serverless.
+                    As part of the Bunch team, I had the opportunity to
+                    contribute to the redesign of the backend infrastructure to
+                    support their improved virtual coaching app. In this role, I
+                    made performance fixes and general improvements to the
+                    platform.
+                  </span>
+                </p>
+                <br />
+                <p class="c11">
+                  <span class="c8">
+                    The backend was built using NodeJS and TypeScript, and it
+                    heavily relied on microservices and serverless architecture.
+                    My experience with these technologies allowed me to quickly
+                    adapt to the project and deliver high-quality results.
+                  </span>
+                </p>{" "}
+                <br />
+                <p class="c11">
+                  <span class="c8">
+                    Overall, my work with Bunch helped to improve the app's
+                    backend infrastructure, providing a more seamless experience
+                    for users seeking to improve their leadership skills. My
+                    skills in NodeJS and TypeScript, coupled with my experience
+                    in microservices and serverless architecture, enabled me to
+                    deliver effective solutions that met the needs of the
+                    project.
                   </span>
                 </p>
               </td>
@@ -281,22 +314,41 @@ export default () => {
                     <br />
                     <br />
                   </span>
-                  <span class="c8">
-                    Joined Baires to work for Instructure Inc as a Node
-                    Developer for Commons, an online platform where people share
-                    content for education. I started helping the team update the
-                    application UI (by moving to Instructure Design components
-                    on React), improving its search (using ElasticSearch and
-                    updating some search queries), implementing new
-                    functionality and giving support to reported bugs.
-                    <br />
-                    <br />A year later I joined their Professional Services
-                    team, now working with Canvas reports and custom tools
-                    developed for Instructure&rsquo;s customers. Here I got to
-                    work with a mix of Rails and React, got to do some
-                    interesting queries for Postgres in order to deliver some
-                    custom reports and got experience working with AWS.
-                  </span>
+                  <p>
+                    <span class="c8">
+                      I began my journey with Instructure Inc through my role as
+                      a Node Developer for Commons, an online platform for
+                      educational content sharing. As part of the development
+                      team at Baires, I helped to update the application's user
+                      interface by migrating to Instructure Design components on
+                      React. Additionally, I leveraged ElasticSearch to enhance
+                      search functionality and implemented new features while
+                      providing support for reported bugs.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      After a year of contributing to the development of
+                      Commons, I joined Instructure's Professional Services
+                      team, where I worked on Canvas reports and custom tools
+                      for Instructure's customers. In this role, I honed my
+                      skills with a mix of Rails and React, while also delving
+                      into the world of Postgres to deliver custom reports.
+                      Working with AWS allowed me to gain valuable experience
+                      with cloud services and infrastructure.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      Throughout my time at Instructure, I demonstrated a strong
+                      work ethic and a commitment to delivering high-quality
+                      results. I was able to collaborate effectively with
+                      cross-functional teams and build solutions that improved
+                      the user experience for our customers.
+                    </span>
+                  </p>
                 </p>
               </td>
             </tr>
@@ -358,39 +410,59 @@ export default () => {
                     <br />
                     <br />
                   </span>
-                  <span class="c8">
-                    Domicilios.com started as a coupon service that saw a
-                    potential in the food delivery area, quickly evolving into a
-                    platform dedicated in solving this problem. I was part of
-                    this transition from the beginning, this allowed me to work
-                    on some of the key parts that helped the company become one
-                    of the most important online food delivery companies in
-                    Colombia.
-                    <br />
-                    <br />
-                    With PHP as its main language and CakePHP as its core
-                    platform, we started from the ground up and as the company
-                    grew, there was a need for an admin panel in order to manage
-                    the platform content which at the time I designed with the
-                    latest tools available: jQuery and Bootstrap 2.
-                    <br />
-                    <br />
-                    As more orders were made, we got a new challenge: We needed
-                    to keep track of the orders, sometimes call the restaurant
-                    and pass it or simply notify them so the customer can get
-                    the food as fast as possible. For this I designed a solution
-                    that was able to assign orders to different agents, trying
-                    to balance the orders by keeping all of the agents with at
-                    least one order but at the same time grouping multiple
-                    orders of the same restaurant so only one call was needed.
-                    <br />
-                    The more the company grew, the more complex the operation
-                    became, I was an active part of the team providing solutions
-                    for the whole company and when we got acquired by
-                    DeliveryHero and more people joined the tech team, I focused
-                    on delivering a new website with an improved experience,
-                    this time working with React more modern tools.
-                  </span>
+                  <p class="c11">
+                    <span class="c8">
+                      At Domicilios.com, I played an instrumental role in the
+                      company's growth from a coupon service to a leading online
+                      food delivery platform in Colombia. From the very
+                      beginning, I was part of the team that oversaw the
+                      transition and worked on key components that contributed
+                      to our success.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      As the company grew, I took on more responsibilities and
+                      designed several critical systems that improved our
+                      operations. For example, I developed an admin panel that
+                      helped us manage platform content and integrated JQuery
+                      and Bootstrap 2 to ensure the panel's efficiency.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      Another significant challenge was managing the influx of
+                      orders, which required a reliable system to notify
+                      restaurants and customers alike. I designed a solution
+                      that could assign orders to different agents while
+                      balancing the workload, ensuring that all agents had at
+                      least one order while grouping multiple orders from the
+                      same restaurant to minimize communication efforts.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      As the company continued to expand and attract more tech
+                      talent, I played an active role in collaborating with the
+                      team to develop solutions that improved our operations.
+                      When we were acquired by DeliveryHero, I led the
+                      development of a new website that delivered a superior
+                      user experience, utilizing modern tools such as React.
+                    </span>
+                  </p>
+                  <br />
+                  <p class="c11">
+                    <span class="c8">
+                      Overall, my contributions to Domicilios.com helped shape
+                      the company's success, and I am proud to have been a part
+                      of such a transformative period in the online food
+                      delivery industry in Colombia.
+                    </span>
+                  </p>
+                  <br />
                 </p>
               </td>
             </tr>
